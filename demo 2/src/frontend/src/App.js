dc84c72c3ed013f0bd0f50abb25f284a8b147baf
scohly <scohly@fordham.edu>
2021-08-25 01:21:58 -0400

    antd configured

--- demo 2/src/frontend/src/App.js
+++ demo 2/src/frontend/src/App.js
@@ -1,11 +1,19 @@
-import {Button} from "antd";
+import {Button,
+        Radio
+} from "antd";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Button type='primary'>Hello</Button>
+        <Button type='primary'>Hello</Button>
+        <br/>
+        <Radio.Group value='large' >
+              <Radio.Button value="large">Large</Radio.Button>
+              <Radio.Button value="default">Default</Radio.Button>
+              <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
 
     </div>
   );
