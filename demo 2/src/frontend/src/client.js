ec6fca2b7354feaa86b275bb661391096b8f09df
scohly <scohly@fordham.edu>
2021-08-25 14:45:15 -0400

    made client.js file

---
+++ demo 2/src/frontend/src/client.js
@@ -0,0 +1,16 @@
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+    //convert non-2xx http response into errors:
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+
+}
+
+export const getAllStudents = () =>
+    fetch("http://localhost:8080/api/v1/students")
+    .then(checkStatus);
